0653efecfed5c3814714289588b7aa277a9e8e12
Xian <34748039+itzXian@users.noreply.github.com>
2022-10-13 06:59:44 +0800

    Update hemlo-text-align.user.js

--- hemlo-text-align.user.js
+++ hemlo-text-align.user.js
@@ -5,7 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://genshin.hemlo.cc/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=hemlo.cc
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=genshin.hemlo.cc
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant        none
