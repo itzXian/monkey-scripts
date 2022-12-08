25fcac9559df4f82c7cb5f0b890933253817ba55
Xian <34748039+itzXian@users.noreply.github.com>
2022-12-08 15:23:43 +0800

    Update gdc-change font.user.js

--- gdc-change font.user.js
+++ gdc-change font.user.js
@@ -11,20 +11,29 @@
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
- waitForKeyElements (".name", Main);
+ waitForKeyElements (".name", Main);
+ waitForKeyElements ("div.main-stat", setArtifactHeight);
+ waitForKeyElements ("div.gi-char-info-name", setCharacterHeight);
 
 function Main() {
-    'use strict';
-    var font = "sans-serif";
-    document.body.style.fontFamily = font;
-    document.body.style.fontWeight = "bold";
-    document.body.style.background = "#2e3440";
-    document.querySelectorAll('.name').forEach(element => {
-        element.style.fontFamily = font;
-        element.style.fontWeight = "bold";
-    });
-    document.querySelectorAll('div.full-height-wrapper').forEach( element => (
-        a.style.maxHeight = "";
-    ));
-    document.querySelector('div.gi-char-list-items-wrapper').style.maxHeight = "";
+    'use strict';
+    var font = "sans-serif";
+    document.body.style.fontFamily = font;
+    document.body.style.fontWeight = "bold";
+    document.body.style.background = "#2e3440";
+    document.querySelectorAll('.name').forEach(element => {
+        element.style.fontFamily = font;
+        element.style.fontWeight = "bold";
+    });
+};
+function setArtifactHeight() {
+    document.querySelectorAll('div.full-height-wrapper').forEach( element => (
+        element.style.maxHeight = ""
+    ));
+}
+function setCharacterHeight() {
+    document.querySelector('div.gi-char-list-items-wrapper').style.maxHeight = "";
+    document.querySelectorAll("input").forEach( element => (
+        element.style.fontFamily = "sans-serif"
+    ))
 };
