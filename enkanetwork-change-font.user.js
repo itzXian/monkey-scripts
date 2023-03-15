4d3d888e239c0d3f72d8a29124c6cb2067c31e84
itzXian <xixian.2106@gmail.com>
2023-03-15 12:42:07 +0800

    update script

--- enkanetwork-change-font.user.js
+++ enkanetwork-change-font.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Enka Change Font
+// @name         enkanetwork-change-font
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
