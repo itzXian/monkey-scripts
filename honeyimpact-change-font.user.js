c139f9d7d565dedff935cbf271ba6f95487a8240
itzXian <xixian.2106@gmail.com>
2023-03-15 12:31:34 +0800

    add new script for HI

---
+++ honeyimpact-change-font.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         honeyimpact-change-font
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://genshin.honeyhunterworld.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=honeyhunterworld.com
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant        none
+// ==/UserScript==
+
+waitForKeyElements(".gsc-input", cF)
+waitForKeyElements(".hamborger_btn_l", btnChange)
+
+function cF() {
+    var a = new Array;
+    a = [".wp-block-post-comments input[type=submit]", "select", "button", ".sort_input", ".search_input", ".sortable", ".stat_table", ".gsc-input"];
+    changeFont(a)
+};
+
+function changeFont(array) {
+    var font = "Iosevka SS09";
+    document.body.style.fontFamily = font;
+    array.forEach( i => {
+    document.querySelectorAll(i).forEach( element => {
+        element.style.fontFamily = font;
+    });
+    });
+}
+
+function btnChange() {
+    var element = document.querySelector(".hamborger_btn_l");
+    element.style.maxWidth = "3rem";
+    element.style.borderRadius = "10px";
+}
