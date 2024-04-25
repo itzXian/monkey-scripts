38bae25655d513ad9050d66c78692442d42ce318
Xian <34748039+itzXian@users.noreply.github.com>
2024-04-25 11:21:52 +0800

    Update honeyimpact-change-font.user.js

--- honeyimpact-change-font.user.js
+++ honeyimpact-change-font.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://genshin.honeyhunterworld.com/*
+// @match        https://gensh.honeyhunterworld.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=honeyhunterworld.com
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
